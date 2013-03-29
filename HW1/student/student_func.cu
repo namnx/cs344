f8b8b93a8bffadd825e8ba07b31b5c3f9a041c3b
namnx <nhamxuannam@yahoo.com>
2013-03-29 23:51:34 +0700

    HW1 commit

--- HW1/student/student_func.cu
+++ HW1/student/student_func.cu
@@ -51,6 +51,14 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+
+  int r = threadIdx.x;
+  for (size_t c = 0; c < numCols; ++c) {
+      uchar4 rgba = rgbaImage[r * numCols + c];
+      float channelSum = .299f * rgba.x + .587f * rgba.y + .114f * rgba.z;
+      greyImage[r * numCols + c] = round(channelSum);
+  }
+
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -58,11 +66,12 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
+  const dim3 blockSize(numRows, 1, 1);  //TODO
   const dim3 gridSize( 1, 1, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
-  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  cudaDeviceSynchronize(); 
+  checkCudaErrors(cudaGetLastError());
 
   /****************************************************************************
   * You can use the code below to help with debugging, but make sure to       *
